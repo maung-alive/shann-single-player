b5d04d7fd869f584db303e15ba0e4c43c923f58d
Maung <maung.alive@gmail.com>
2024-03-20 19:50:10 +0630

    add backend

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from game.Player import Player
+from game.Computer import Computer
+from game.Table import ShanTable
+from game.Deck import Deck
+
 # Create your views here.
 def home(request):
     return render(request, 'core/index.html', {})
@@ -8,7 +13,17 @@ def play(request):
     if request.method == "POST":
         username = request.POST.get('username')
         request.session["username"] = username
-        return render(request, 'core/play.html', {
-            'error_message': "You're logged in as " + username
-        })
+        players = []
+        player = Player(username)
+        players.append(player)
+        for i in range(6):
+            computer = Computer("Computer " + str(i))
+            players.append(computer)
+        
+        table = ShanTable(players, Deck())
+        json = table.convert_json()
+        print(json)
+        request.session["game"] = json
+
+        return render(request, 'core/play.html', {})
     return render(request, 'core/play.html', {})
