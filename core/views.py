c00fbde8e9a97929141364910b6ba2c0dbb3aeb5
Maung <maung.alive@gmail.com>
2024-03-20 18:20:50 +0630

    add new view, play

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/index.html', {})
+
+def play(request):
+    return render(request, 'core/play.html', {})
