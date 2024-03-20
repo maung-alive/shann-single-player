fe1d8965e1af56a423bc74f30bffd716bf79e9f0
Maung <maung.alive@gmail.com>
2024-03-20 19:09:12 +0630

    handle post request

--- core/views.py
+++ core/views.py
@@ -5,4 +5,8 @@ def home(request):
     return render(request, 'core/index.html', {})
 
 def play(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        request.session["username"] = username
+        return render(request, 'core/play.html', {})
     return render(request, 'core/play.html', {})
