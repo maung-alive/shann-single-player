4802154108e945548a1313494e09f7448b116160
Maung <maung.alive@gmail.com>
2024-03-20 17:55:54 +0630

    add flame gif

---
+++ static/script.js
@@ -0,0 +1,19 @@
+const cards = ['10_of_clubs.png', '10_of_diamonds.png', '10_of_hearts.png', '10_of_spades.png', '2_of_clubs.png', '2_of_diamonds.png', '2_of_hearts.png', '2_of_spades.png', '3_of_clubs.png', '3_of_diamonds.png', '3_of_hearts.png', '3_of_spades.png', '4_of_clubs.png', '4_of_diamonds.png', '4_of_hearts.png', '4_of_spades.png', '5_of_clubs.png', '5_of_diamonds.png', '5_of_hearts.png', '5_of_spades.png', '6_of_clubs.png', '6_of_diamonds.png', '6_of_hearts.png', '6_of_spades.png', '7_of_clubs.png', '7_of_diamonds.png', '7_of_hearts.png', '7_of_spades.png', '8_of_clubs.png', '8_of_diamonds.png', '8_of_hearts.png', '8_of_spades.png', '9_of_clubs.png', '9_of_diamonds.png', '9_of_hearts.png', '9_of_spades.png', 'ace_of_clubs.png', 'ace_of_diamonds.png', 'ace_of_hearts.png', 'ace_of_spades.png', 'jack_of_clubs.png', 'jack_of_diamonds.png', 'jack_of_hearts.png', 'jack_of_spades.png', 'king_of_clubs.png', 'king_of_diamonds.png', 'king_of_hearts.png', 'king_of_spades.png', 'queen_of_clubs.png', 'queen_of_diamonds.png', 'queen_of_hearts.png', 'queen_of_spades.png']
+const playButton = document.querySelector(".play-btn");
+
+playButton.addEventListener('click', () => {
+    let text = document.querySelector('.welcome');
+    text.className += " wrinkles"
+    playButton.innerHTML = "စောင့်ပါ"
+    let progress = document.querySelector(".progress-value");
+    const images = [];
+    for (let i = 0; i < cards.length; i++) {
+        let temp = new Image()
+        images.push(temp)
+        temp.src = `static/images/cards/${cards[i]}`
+        temp.addEventListener('load', () => {
+            document.querySelector('.progress-percent').innerHTML = `${Math.floor((i/cards.length) * 100)}%`;
+            progress.style.width = `${Math.floor((i/cards.length) * 100)}%`;
+        })
+    }
+})
